06d745111255c4cc9b83d9674acad207125765c4
Yuji Sato <yujisatojr@gmail.com>
2024-04-08 22:11:26 -0500

    Test script from jupyter

--- app.py
+++ app.py
@@ -1,99 +1,190 @@
+import pandas as pd
+import os
+# import logging
+from openai import OpenAI
+import openai
+from tqdm import tqdm
 from dotenv import load_dotenv, find_dotenv
+from qdrant_client import models, QdrantClient
+from qdrant_client.http import models as rest
+from qdrant_client.http.models import Record
+from sentence_transformers import SentenceTransformer
+
 load_dotenv(find_dotenv())
-
-from langchain.llms import OpenAI
-llm = OpenAI(model_name="gpt-3.5-turbo-instruct")
-llm("explain large language models in one sentence")
-
-from langchain.schema import (
-    AIMessage,
-    HumanMessage,
-    SystemMessage
-)
-from langchain.chat_models import ChatOpenAI
-
-chat = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.3)
-messages = [
-    SystemMessage(content="You are an expert data scientist"),
-    HumanMessage(content="Write a Python script that trains a neural network on simulated data")
-]
-response = chat(messages)
-print(response.content, end='\n')
-
-from langchain import PromptTemplate
-
-template = """
-You are an expert data scientist with an expertise in building deep learning models.
-Explain the concept of {concept} in a couple of lines
-"""
-
-prompt = PromptTemplate(
-    input_variables=["concept"],
-    template=template,
-)
-print(prompt)
-
-llm(prompt.format(concept="regularization"))
-
-from langchain.chains import LLMChain
-chain = LLMChain(llm=llm, prompt=prompt)
-
-# Run the chain only specifying the input variable.
-print(chain.invoke("autoencoder"))
-
-second_prompt = PromptTemplate(
-    input_variables=["ml_concept"],
-    template="Turn the concept description of {ml_concept} and explain it to me like I'm five",
-)
-chain_two = LLMChain(llm=llm, prompt=second_prompt)
-
-from langchain.chains import SimpleSequentialChain
-overall_chain = SimpleSequentialChain(chains=[chain, chain_two], verbose=True)
-
-# Run the chain specifying only the input variable for the first chain.
-explanation = overall_chain.invoke("autoencoder")
-print(explanation)
-
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-
-text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size = 100,
-    chunk_overlap = 0,
+client = OpenAI(
+    api_key=os.getenv('OPENAI_API_KEY'),
 )
 
-explanation_text = explanation.get("output", "")  # Replace "text" with the key containing the text
-texts = text_splitter.create_documents([explanation_text])
+# Read the CSV dataset
+file_path = './datasets/movies_embedding.csv'
+df = pd.read_csv(file_path) # Add .head(100) if you want to limit the number of rows
 
-print(texts)
-print(texts[0].page_content)
+print(str(len(df)) + ' rows')
+print(df.head(5))
 
-from langchain.embeddings import OpenAIEmbeddings
+# Initialize the Qdrant client
+qdrant_client = QdrantClient(':memory:')
 
-embeddings = OpenAIEmbeddings(model="text-embedding-ada-002")
+# Set the collection name and size
+collection_name = 'movies'
+vector_size = len(df['metadata_vector'][0])
 
-query_result = embeddings.embed_query(texts[0].page_content)
-print(query_result)
-
-from qdrant_client import QdrantClient
-
-qdrant_client = QdrantClient("localhost", port=6333)
-
-from qdrant_client.http.models import Distance, VectorParams
-
-qdrant_client.create_collection(
-    collection_name="test",
-    vectors_config=VectorParams(size=4, distance=Distance.DOT),
+# Create a collection
+qdrant_client.recreate_collection(
+    collection_name=collection_name,
+    vectors_config={
+        'metadata': rest.VectorParams(
+            distance=rest.Distance.COSINE,
+            size=vector_size,
+        ),
+    }
 )
 
-# Define a function to store embeddings in Qdrant
-def store_embeddings_in_qdrant(embeddings, object_ids):
-    vectors = [{'vector': vector, 'object_id': obj_id} for vector, obj_id in zip(embeddings, object_ids)]
-    upsert_request = client.upsert(
-        collection_name="test",  # Name of your collection
-        vectors=vectors
+# Add vectors to the collection
+qdrant_client.upsert(
+    collection_name=collection_name,
+    points=[
+        rest.PointStruct(
+            id=k,
+            vector={
+                'metadata': v['metadata_vector'],
+            },
+            payload=v.to_dict(),
+        )
+        for k, v in df.iterrows()
+    ],
+)
+
+print(qdrant_client.get_collections())
+qdrant_client.count(collection_name=collection_name)
+
+# Generate a query embedding and search in Qdrant
+def query_qdrant(query, collection_name, vector_name, top_k=20):
+    # Creates embedding vector from user query
+    completion = openai.embeddings.create(
+        input=query,
+        model='text-embedding-3-small'  # Be sure to use the same embedding model as the vectors in the collection
     )
-    client.upsert_entity(upsert_request)
 
-# Insert embeddings in qdrant vector database
-object_ids = [i for i in range(len(embeddings))]
-store_embeddings_in_qdrant(embeddings, object_ids)
+    embedded_query = completion.data[0].embedding
+
+    query_results = qdrant_client.search(
+        collection_name=collection_name,
+        query_vector=(
+            vector_name, embedded_query
+        ),
+        limit=top_k,
+    )
+    
+    return query_results
+
+# Filter by conditions in Qdrant
+def filter_qdrant(adult, collection_name, vector_name, top_k=5):
+    query_results = qdrant_client.scroll(
+        collection_name=collection_name,
+        scroll_filter=models.Filter(
+            must=[
+                # models.FieldCondition(
+                #     key="year",
+                #     match=models.MatchValue(value=year),
+                # ),
+                models.FieldCondition(
+                    key="adult",
+                    match=models.MatchValue(value=adult),
+                ),
+            ]
+        ),
+    )
+    return query_results
+
+# query_results = filter_qdrant(adult=False, collection_name=collection_name, vector_name='metadata')
+
+# print(query_results)
+
+# for i, vector in enumerate(query_results):
+#     # print(vector[0])
+#     print(vector[i].payload['title'])
+
+# Format the response as JSON
+import json
+from datetime import datetime
+import locale
+
+# Assume year in 2000's if between 00 to 24, otherwise, in 1900's
+def convert_date_format(date_str):
+    month, day, year = map(int, date_str.split('/'))
+    
+    if year >= 0 and year <= 24:
+        year += 2000
+    else:
+        year += 1900
+
+    date_obj = datetime(year, month, day)
+    formatted_date = date_obj.strftime('%m/%d/%Y')
+    
+    return formatted_date
+
+def string_to_array(str):
+    arr = str.split(',')
+    arr = [arr.strip() for arr in arr]
+
+    return arr
+
+def format_time(minutes_float):
+    minutes_int = int(minutes_float)
+
+    hours = minutes_int // 60
+    minutes = minutes_int % 60
+    
+    if hours > 0:
+        time_string = f"{hours}h {minutes}m"
+    else:
+        time_string = f"{minutes}m"
+    
+    return time_string
+
+def format_as_dollars(number):
+    locale.setlocale(locale.LC_ALL, '')
+
+    number = int(number)
+    formatted_number = locale.currency(number, grouping=True)
+    formatted_number = formatted_number.replace(locale.localeconv()['currency_symbol'], "$")
+
+    return formatted_number
+
+def get_country_name(abbreviation):
+    country_names = {'en': 'English', 'fr': 'French', 'zh': 'Chinese', 'it': 'Italian', 'fa': 'Persian', 'nl': 'Dutch', 'de': 'German', 'cn': 'Chinese', 'ar': 'Arabic', 'es': 'Spanish', 'ru': 'Russian', 'sv': 'Swedish', 'ja': 'Japanese', 'ko': 'Korean', 'sr': 'Serbian', 'bn': 'Bengali', 'he': 'Hebrew', 'pt': 'Portuguese', 'wo': 'Wolof', 'ro': 'Romanian', 'hu': 'Hungarian', 'cy': 'Welsh', 'vi': 'Vietnamese'}
+    return country_names.get(abbreviation, '')
+
+# Function to search for similar vectors
+def search_movies_in_qdrant(query):
+    query_results = query_qdrant(query, collection_name, 'metadata')
+
+    results = []
+    
+    for i, vector in enumerate(query_results):
+        tmp = {
+            "rank": i,
+            "title": vector.payload["title"],
+            "summary": vector.payload["summary"],
+            "date": convert_date_format(vector.payload["date"]),
+            "genres": string_to_array(vector.payload["genres"]),
+            "runtime": format_time(vector.payload["runtime"]),
+            "rating": vector.payload["rating"],
+            "votes": int(vector.payload["votes"]),
+            "budget": format_as_dollars(vector.payload["budget"]),
+            "revenue": format_as_dollars(vector.payload["revenue"]),
+            "language": get_country_name(vector.payload["language"]),
+            "adult": vector.payload["adult"],
+            "production": string_to_array(vector.payload["production"]),
+            "poster_link": vector.payload["poster_link"]
+        }
+        results.append(tmp)
+
+    return results
+
+query = 'scary movies about monsters after year 2003'
+response = (search_movies_in_qdrant(query))
+
+json_string = json.dumps(response, indent=2)
+print(json_string)
