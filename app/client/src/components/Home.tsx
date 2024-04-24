1d3fdee3cb19db681329aa8cbb09686050f9575f
Yuji Sato <yujisatojr@gmail.com>
2024-04-23 23:18:46 -0500

    Reorg App.tsx components into Home and MovieCard

---
+++ app/client/src/components/Home.tsx
@@ -0,0 +1,394 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import FadeIn from 'react-fade-in';
+import { Oval, ThreeDots } from 'react-loader-spinner'
+import { TypeAnimation } from 'react-type-animation';
+import MovieCard from './MovieCard';
+import '../assets/styles/Home.scss';
+
+// Import MUI components
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import Alert from '@mui/material/Alert';
+import Collapse from '@mui/material/Collapse';
+import FormControl from '@mui/material/FormControl';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Grid from '@mui/material/Grid';
+import InputBase from '@mui/material/InputBase';
+import IconButton from '@mui/material/IconButton';
+import Paper from '@mui/material/Paper';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import { SelectChangeEvent } from '@mui/material/Select';
+
+// Import icons
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import CloseIcon from '@mui/icons-material/Close';
+import HelpIcon from '@mui/icons-material/Help';
+import SearchIcon from '@mui/icons-material/Search';
+
+function Home() {
+	const [searchInput, setSearchInput] = useState<string>('');
+	const [filterData, setFilterData] = useState<any>(null);
+	const [movieData, setMovieData] = useState<any>(null);
+	const [movieDetail, setMovieDetail] = useState<any>(null);
+
+	const [isFilterLoading, setIsFilterLoading] = useState<boolean>(false);
+	const [open, setOpen] = useState<boolean>(false);
+	const [clicked, setClicked] = useState<boolean>(false);
+
+	const [selectedGenre, setGenre] = useState('All');
+	const [selectedCertificate, setCertificate] = useState('All');
+	
+	const genres = [
+		'All', 'Action', 'Adventure', 'Animation', 'Biography', 'Comedy', 'Crime', 
+		'Documentary', 'Drama', 'Family', 'Fantasy', 'History', 'Horror', 
+		'Musical', 'Mystery', 'Romance', 'Sci-Fi', 'Sports', 'Thriller', 
+		'War', 'Western'
+	];
+  
+	const certificates = [
+		'All', 'G', 'PG', 'TV-PG', 'PG-13', 'TV-14', 'R', 'TV-MA'
+	];
+
+	// Change state based on the values from child components
+	const handleMovieChange = (data: any) => {
+		setMovieDetail(data);
+		// setClicked(true);
+		window.scrollTo({
+			top: 0,
+			behavior: 'smooth'
+		});
+	};
+
+	const handleClickedChange = () => {
+		setClicked(false)
+	};
+		
+	const handleGenreChange = (event: SelectChangeEvent) => {
+		setGenre(event.target.value as string);
+	};
+	
+	const handleCertChange = (event: SelectChangeEvent) => {
+		setCertificate(event.target.value as string);
+	};
+
+	const handleSearchSubmit = async () => {
+		setClicked(false);
+		setIsFilterLoading(true);
+		try {
+			const response = await fetch(`/generate_filters?user_query=${searchInput}`);
+			if (response.ok) {
+				const data = await response.json();
+				setFilterData(await data);
+				setIsFilterLoading(false);
+			} else {
+				console.error('Error fetching movie data');
+				setIsFilterLoading(false);
+			}
+		} catch (error) {
+			console.error('Error fetching movie data:', error);
+			setIsFilterLoading(false);
+		}
+	};
+		
+	const handleClick = (movie: any) => {
+		setMovieDetail(movie);
+		setClicked(true);
+	};
+
+	const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setSearchInput(e.target.value);
+	};
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const response = await axios.post('/search_movies', filterData);
+				setMovieData(response.data);
+			} catch (error) {
+				console.log(error);
+			}
+		};
+
+		if (filterData !== null) {
+			fetchData();
+		}
+
+		return () => {
+			// Cancel any ongoing requests here if needed
+		};
+	}, [filterData]);
+
+	useEffect(() => {
+		window.scrollTo({
+			top: 0,
+			behavior: 'smooth'
+		});
+	}, [clicked]);
+
+	useEffect(() => {
+		handleSearchSubmit();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
+
+  	return (
+	<div className='movie_app_root'>
+		<FadeIn transitionDuration={700}>
+			<Grid container spacing={2}>
+				<Grid item xs={12} sm={12} md={3.5} lg={3.5} xl={3.5} className='sidebar_filter'>
+					<div className='search_form_wrapper'>
+						<Paper
+						className='search_form'
+						component="form"
+						sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: '100%' }}
+						onSubmit={(e) => {
+							e.preventDefault();
+							handleSearchSubmit();
+						}}
+						>
+							<InputBase
+							className='input_form'
+							sx={{ ml: 1, flex: 1 }}
+							placeholder='Search with natural language query'
+							inputProps={{ 'aria-label': 'search movies' }}
+							value={searchInput}
+							onChange={handleInputChange}
+							/>
+							<IconButton type="submit" sx={{ p: '10px' }} aria-label="search">
+								<SearchIcon />
+							</IconButton>
+						</Paper>
+					</div>
+					<Accordion className='accordion_container'>
+						<AccordionSummary
+						expandIcon={<ArrowDropDownIcon />}
+						aria-controls="panel2-content"
+						id="panel2-header"
+						>
+						{isFilterLoading ? (
+						<span className='accordion_label'>
+							<Oval
+							visible={true}
+							height="20"
+							width="20"
+							color="black"
+							secondaryColor="gray"
+							ariaLabel="oval-loading"
+							wrapperStyle={{}}
+							wrapperClass=""
+							strokeWidth={5}
+							/>
+							Generating recommends based on input...
+						</span>
+						) : (
+						<span className='accordion_label'>
+							<CheckCircleIcon/> 
+							Finished generating recommends!
+						</span>
+						)}
+						</AccordionSummary>
+						<AccordionDetails>
+						{(!isFilterLoading && filterData) ? (
+						<Alert
+						className='alert_container filter_list'
+						severity="info"
+						sx={{ mb: 2 }}
+						>
+							<TypeAnimation
+							sequence={[
+								`${filterData['sentiment'] === 'positive' ? 'The search results have been tailored to include only movies that align with the positive emotions based on your input.\n' : ''}
+								${filterData['sentiment'] === 'negative' ? 'The search results have been tailored to include only movies that align with the negative emotions based on your input.\n': ''}
+								${filterData['insights'] !== '' ? filterData['insights'] : ''}`,
+							]}
+							speed={{ type: 'keyStrokeDelayInMs', value: 30 }}
+							style={{ fontSize: '1em', display: 'block'}}
+							cursor={false}
+							/>
+						</Alert>
+						) : (<span>Loading...</span>)}
+						</AccordionDetails>
+					</Accordion>
+
+					<Accordion className='accordion_container'>
+						<AccordionSummary
+						expandIcon={<ArrowDropDownIcon />}
+						aria-controls="panel2-content"
+						id="panel2-header"
+						>
+						Content Rating
+						</AccordionSummary>
+						<AccordionDetails>
+						<FormControl>
+							<RadioGroup
+							row
+							aria-labelledby="radio-buttons-group-label"
+							name="row-radio-buttons-group"
+							onChange={handleCertChange}
+							defaultValue='All'
+							>
+							{certificates.map(cert => (
+								<FormControlLabel key={cert} value={cert} control={<Radio />} label={cert} />
+							))}
+							</RadioGroup>
+						</FormControl>
+						</AccordionDetails>
+					</Accordion>
+
+					<Accordion className='accordion_container'>
+						<AccordionSummary
+						expandIcon={<ArrowDropDownIcon />}
+						aria-controls="panel2-content"
+						id="panel2-header"
+						>
+						Genre
+						</AccordionSummary>
+						<AccordionDetails>
+						<FormControl>
+							<RadioGroup
+							row
+							aria-labelledby="radio-buttons-group-label"
+							name="row-radio-buttons-group"
+							onChange={handleGenreChange}
+							defaultValue='All'
+							>
+							{genres.map(genre => (
+								<FormControlLabel key={genre} value={genre} control={<Radio />} label={genre} />
+							))}
+							</RadioGroup>
+						</FormControl>
+						</AccordionDetails>
+					</Accordion>
+
+					<Accordion className='accordion_container'>
+						<AccordionSummary
+						expandIcon={<ArrowDropDownIcon />}
+						aria-controls="panel2-content"
+						id="panel2-header"
+						>
+						Rating
+						</AccordionSummary>
+						<AccordionDetails>
+						
+						</AccordionDetails>
+					</Accordion>
+
+					<Accordion className='accordion_container'>
+						<AccordionSummary
+						expandIcon={<ArrowDropDownIcon />}
+						aria-controls="panel2-content"
+						id="panel2-header"
+						>
+						Runtime
+						</AccordionSummary>
+						<AccordionDetails>
+						
+						</AccordionDetails>
+					</Accordion>
+
+					<Accordion className='accordion_container'>
+						<AccordionSummary
+						expandIcon={<ArrowDropDownIcon />}
+						aria-controls="panel2-content"
+						id="panel2-header"
+						>
+						Sentiment
+						</AccordionSummary>
+						<AccordionDetails>
+						
+						</AccordionDetails>
+					</Accordion>
+
+					<Accordion className='accordion_container'>
+						<AccordionSummary
+						expandIcon={<ArrowDropDownIcon />}
+						aria-controls="panel2-content"
+						id="panel2-header"
+						>
+						Year
+						</AccordionSummary>
+						<AccordionDetails>
+						
+						</AccordionDetails>
+					</Accordion>
+				</Grid>
+				
+				<Grid className='right_container' item xs={12} sm={12} md={8.5} lg={8.5} xl={8.5}>
+					<Collapse in={open}>
+						<Alert
+						className='alert_container'
+						severity="info"
+						icon={<HelpIcon fontSize="inherit" />}
+						sx={{ mb: 2 }}
+						action={
+							<IconButton
+							aria-label="close"
+							color="inherit"
+							size="small"
+							onClick={() => {setOpen(false);}}>
+								<CloseIcon fontSize="inherit" />
+							</IconButton>
+						}>
+							<span className='instruction'>This movie search app uses GenAI to handle natural language queries beyond the traditional keyword search. The app also provides recommendations on movie titles and directors based on the input sentences. Try out the following three search patterns:</span>
+							<ol>
+								<li>Semantic search based on context and concepts: For example, using the word "Ocean" will yield hits related to summer, ships, and blue, etc.</li>
+								<li>Sentiment analysis based on emotion: For example, "happy movie" and "sad movie" will sort movies based on their content mood.</li>
+								<li>Filter search based on words or phrases: It supports flexible searches including movie titles, director names, content, and revenues.</li>
+							</ol>
+						</Alert>
+					</Collapse>
+
+					{isFilterLoading && (
+					<FadeIn transitionDuration={500}>
+						<div className='loading'>
+							<ThreeDots
+							visible={true}
+							height="60"
+							width="60"
+							color="white"
+							radius="9"
+							ariaLabel="three-dots-loading"
+							wrapperStyle={{}}
+							wrapperClass=""
+							/>
+						</div>
+					</FadeIn>
+					)}
+
+					{!isFilterLoading && !clicked && (
+					<FadeIn transitionDuration={700}>
+						<Grid container spacing={2} className='result_container'>
+							<Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+								<Grid container spacing={2}>
+								{movieData && movieData.map((movie: any, index: number) => (
+									<Grid item xs={12} sm={12} md={3} lg={3} xl={3} key={index}>
+										<FadeIn transitionDuration={700} key={index}>
+											<div key={index} className='movie_img zoom' onClick={() => handleClick(movie)}>
+												<img className='image_fill' alt={movie.title} src={movie.img}/>
+											</div>
+										</FadeIn>
+									</Grid>
+								))}
+								</Grid>
+							</Grid>
+						</Grid>
+					</FadeIn>
+					)}
+
+					<React.StrictMode>
+						<MovieCard
+						parentToChild={{ movieDetail, isFilterLoading, clicked }}
+						movieChange={handleMovieChange}
+						clickedChange={handleClickedChange}
+						/>
+					</React.StrictMode>
+				</Grid>
+			</Grid>
+		</FadeIn>
+	</div>
+  );
+};
+  
+export default Home;
---
+++ app/client/src/components/MovieCard.tsx
@@ -0,0 +1,279 @@
+import React, { useEffect, useState } from 'react';
+import FadeIn from 'react-fade-in';
+import { styled } from '@mui/material/styles';
+
+// Import MUI components
+// import Divider from '@mui/material/Divider';
+import Checkbox from '@mui/material/Checkbox';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Rating from '@mui/material/Rating';
+import Skeleton from '@mui/material/Skeleton';
+
+// Import icons
+import ArrowRightIcon from '@mui/icons-material/ArrowRight';
+import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
+import BookmarkIcon from '@mui/icons-material/Bookmark';
+import CloseIcon from '@mui/icons-material/Close';
+import FavoriteBorder from '@mui/icons-material/FavoriteBorder';
+import Favorite from '@mui/icons-material/Favorite';
+import SentimentSatisfiedIcon from '@mui/icons-material/SentimentSatisfied';
+
+function convertToRating(sentimentScore: number) {
+    // Normalize the range from -1 to 1 to 0 to 2
+    var normalizedValue = sentimentScore + 1;
+    
+    // Scale the range from 0 to 2 to 0 to 5
+    var newValue = normalizedValue * 2.5;
+    newValue = Math.round(newValue * 10) / 10;
+    
+    return newValue;
+}
+
+function MovieCard({ parentToChild, movieChange, clickedChange }: any) {
+
+    const {movieDetail, isFilterLoading, clicked} = parentToChild;
+
+    const [similarMoviesData, setSimilarMoviesData] = useState<any>(null);
+    const [castsData, setCastsData] = useState<any>(null);
+    const [newsData, setNewsData] = useState<any>(null);
+    // const [trailerData, setTrailerData] = useState<any>(null);
+    // const [trailerError, setTrailerError] = useState<boolean>(false);
+
+    const [isSimilarLoading, setIsSimilarLoading] = useState<boolean>(false);
+    const [isCastsLoading, setIsCastsLoading] = useState<boolean>(false);
+    const [isNewsLoading, setIsNewsLoading] = useState<boolean>(false);
+
+    const Item = styled(Paper)(({ theme }) => ({
+        padding: theme.spacing(1),
+        textAlign: 'left',
+    }));
+
+    const StyledRating = styled(Rating)({  
+        '& .MuiRating-iconFilled': {
+          color: '#ff6d75',
+        },
+    });
+
+    useEffect(() => {
+        setIsCastsLoading(true);
+        const fetchData = async () => {
+            try {
+                const response = await fetch(`/generate_casts?title=${movieDetail.title}(${movieDetail.year})`);
+                if (response.ok) {
+                    const data = await response.json();
+                    setCastsData(await data);
+                    setIsCastsLoading(false);
+                } else {
+                    console.error('Error fetching movie casts data');
+                    setIsCastsLoading(false);
+                }
+            } catch (error) {
+                console.error('Error fetching movie casts data:', error);
+                setIsCastsLoading(false);
+            }
+        };
+
+        if (movieDetail !== null) {
+            fetchData();
+        }
+    }, [movieDetail]);
+
+    useEffect(() => {
+        setIsNewsLoading(true);
+        const fetchData = async () => {
+            try {
+                const response = await fetch(`/generate_news?title=${movieDetail.title}(${movieDetail.year})`);
+                if (response.ok) {
+                    const data = await response.json();
+                    setNewsData(await data);
+                    setIsNewsLoading(false);
+                } else {
+                    console.error('Error fetching movie news data');
+                    setIsNewsLoading(false);
+                }
+            } catch (error) {
+                console.error('Error fetching movie news data:', error);
+                setIsNewsLoading(false);
+            }
+        };
+
+        if (movieDetail !== null) {
+            fetchData();
+        }
+    }, [movieDetail]);
+
+    // useEffect(() => {
+    //   const fetchData = async () => {
+    //     try {
+    //       const response = await fetch(`/generate_trailer?title=${movieDetail.title}`);
+    //       if (response.ok) {
+    //         const data = await response.json();
+    //         setTrailerData(await data);
+    //       } else {
+    //         console.error('Error fetching movie trailer data');
+    //       }
+    //     } catch (error) {
+    //       console.error('Error fetching movie trailer data:', error);
+    //     }
+    //   };
+
+    //   if (movieDetail !== null) {
+    //     fetchData();
+    //   }
+    // }, [movieDetail]);
+    
+    useEffect(() => {
+        setIsSimilarLoading(true);
+        const fetchData = async () => {
+            try {
+                const response = await fetch(`/similarity_search?metadata=${movieDetail.metadata}`);
+            if (response.ok) {
+                const data = await response.json();
+                setSimilarMoviesData(await data);
+                setIsSimilarLoading(false);
+            } else {
+                console.error('Error fetching movie data');
+                setIsSimilarLoading(false);
+            }
+            } catch (error) {
+                console.error('Error fetching movie data:', error);
+                setIsSimilarLoading(false);
+            }
+        };
+
+        if (movieDetail !== null) {
+            fetchData();
+        }
+    }, [movieDetail]);
+
+    return (
+        <>
+        {!isFilterLoading && clicked && (
+		<FadeIn transitionDuration={700}>
+			<Grid container spacing={2} className='result_container'>
+                <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                    <Item className='movie_detail_card'>
+                        <Grid container spacing={2}>
+                            <Grid item xs={12} sm={12} md={4} lg={4} xl={4}>
+                                <img className='image_fill' alt={movieDetail.title} src={movieDetail.img}/>
+                                <div className='checkbox_elements'>
+                                    <Checkbox icon={<FavoriteBorder />} checkedIcon={<Favorite className='heart_icon'/>} />
+                                    <Checkbox icon={<BookmarkBorderIcon />} checkedIcon={<BookmarkIcon className='save_icon' />} />
+                                </div>
+                            </Grid>
+                            <Grid className='right_area' item xs={12} sm={12} md={8} lg={8} xl={8}>
+                                <div className='right_header'>
+                                    <h1>
+                                        {movieDetail.title}
+                                        <span> ({movieDetail.year})</span>
+                                    </h1>
+                                    <CloseIcon fontSize="inherit" onClick={() => clickedChange(false)}/>
+                                </div>
+                                
+                                <div className='detail_section'>
+                                    <p className='sub_section'>{movieDetail.genre}</p>
+                                    <p className='sub_section'>{movieDetail.runtime}</p>
+                                    <p>{movieDetail.certificate}</p>
+                                </div>
+                                
+                                <p>{movieDetail.summary}</p>
+
+                                <div className='flex_section'>
+                                    <div className='sub_section'>
+                                        <h3>Rating</h3>
+                                        <Rating name="rating_star" value={movieDetail.rating} precision={0.1} max={10} readOnly />
+                                        <p>{movieDetail.rating}/10 ({movieDetail.votes} votes)</p>
+                                    </div>
+
+                                    <div>
+                                        <h3>Sentiment Score</h3>
+                                        <StyledRating
+                                        name="customized-color"
+                                        className={
+                                        (movieDetail.sentiment < -0.7) ? 'very_low' :
+                                        (movieDetail.sentiment >= -0.7 && movieDetail.sentiment < -0.3) ? 'low' :
+                                        (movieDetail.sentiment >= -0.3 && movieDetail.sentiment < 0.3) ? 'neutral' :
+                                        (movieDetail.sentiment >= 0.3 && movieDetail.sentiment < 0.7) ? 'high' :
+                                        (movieDetail.sentiment >= 0.7 && movieDetail.sentiment <= 1) ? 'very_high' :
+                                        ''
+                                        }
+                                        value={convertToRating(movieDetail.sentiment)}
+                                        precision={0.1}
+                                        max={5}
+                                        icon={<SentimentSatisfiedIcon fontSize="inherit" />}
+                                        emptyIcon={<SentimentSatisfiedIcon fontSize="inherit" />}
+                                        style={{ color: 'blue' }}
+                                        readOnly
+                                        />
+                                        <p>{convertToRating(movieDetail.sentiment)}/5</p>
+                                    </div>
+                                </div>
+
+                                {/* {(trailerData && !trailerError) ? (
+                                    <ReactPlayer url={trailerData.url} onError={(e) => {
+                                    e.preventDefault()
+                                    setTrailerError(true)
+                                    }}/>
+                                ) : (
+                                    <ReactPlayer url='https://youtu.be/dQw4w9WgXcQ?si=hJge3e8INVEqXkvK'/>
+                                )} */}
+
+                                {!isCastsLoading ? (
+                                    castsData && castsData.director !== 'Unknown' && castsData.director !== '' && (
+                                        <div className='padding-bottom'>
+                                        <h3>Top Crew</h3>
+                                        <p>Director: {castsData.director}</p>
+                                        <p>Writer: {castsData.writer}</p>
+                                        <p>Main Casts: {castsData.main_cast_1 !== '' && castsData.main_cast_1} | {castsData.main_cast_2 !== '' && castsData.main_cast_2} | {castsData.main_cast_3 !== '' && castsData.main_cast_3}</p>
+                                        </div>
+                                    )
+                                ) : (
+                                    <Skeleton variant="rounded" width="100%" height={150} style={{marginBottom: '15px', backgroundColor: 'rgb(124 124 124)'}} />
+                                )}
+
+                                {!isNewsLoading ? (
+                                    newsData && newsData.headline_1 !== '' && (
+                                        <div className='movie_news padding-bottom'>
+                                        <h3>Related News</h3>
+                                        <p><ArrowRightIcon/> {newsData.headline_1}</p>
+                                        <p><ArrowRightIcon/> {newsData.headline_2}</p>
+                                        <p><ArrowRightIcon/> {newsData.headline_3}</p>
+                                        </div>
+                                    )
+                                ) : (
+                                    <Skeleton variant="rounded" width="100%" height={150} style={{marginBottom: '15px', backgroundColor: 'rgb(124 124 124)'}} />
+                                )}
+
+                                {!isSimilarLoading ? (
+                                <>
+                                    <h3>You may also like:</h3>
+                                    <Grid container spacing={2}>
+                                        {similarMoviesData && similarMoviesData.map((movie: any, index: number) => (
+                                        <Grid item xs={12} sm={12} md={2.4} lg={2.4} xl={2.4} key={index}>
+                                            <div key={index} className='movie_img zoom' onClick={() => movieChange(movie)}>
+                                                <img
+                                                className='image_fill'
+                                                alt={movie.title}
+                                                src={movie.img}
+                                                />
+                                            </div>
+                                        </Grid>
+                                        ))}
+                                    </Grid>
+                                </>
+                                ) : (
+                                    <Skeleton variant="rounded" width="100%" height={150} style={{marginBottom: '15px', backgroundColor: 'rgb(124 124 124)'}} />
+                                )}
+                            </Grid>
+                        </Grid>
+                    </Item>
+                </Grid>
+			</Grid>
+		</FadeIn>
+        )}
+        </>
+    );
+};
+
+export default MovieCard;
